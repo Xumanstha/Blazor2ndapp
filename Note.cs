00b7cb7d87ef05d9514b2fa4048b1728400a40d2
Xumanstha <shresthaangura@gmail.com>
2025-01-01 13:23:52 +0545

    third commit

--- Note.cs
+++ Note.cs
@@ -1,12 +1,10 @@
 
 public class NoteItem
 {
-    public string? Title{get;set;}
-    public string? Descriptions{get;set;}
-   public bool IsEdit{get;set;}=false;
-    public NoteItem(string T,string D)
-    {
-        Title=T;
-        Descriptions=D;
-    }
+    public string? Title { get; set; }
+    public string? Descriptions { get; set; }
+    public bool IsEdit { get; set; } = false;
+
+
+    public int counter { get; set; } = 0;
 }
